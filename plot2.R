ebdbe9650c8bbb349871f717929388bc5ad8e12f
Francois Vallieres <vallieresf@gmail.com>
2015-02-06 11:24:54 -0500

    Corrections for the dataframe name

--- plot2.R
+++ plot2.R
@@ -5,6 +5,6 @@ household$DateTime <- strptime(household$DateTime, "%Y-%m-%d %H:%M:%S")
 
 
 png("figure/plot2.png", width=480, height=480)
-plot(subHouseHold$DateTime, subHouseHold$Global_active_power, type="n", xlab="", ylab="Global Active Power (kilowatts)") 
-lines(subHouseHold$DateTime, subHouseHold$Global_active_power, type="l")
+plot(household$DateTime, household$Global_active_power, type="n", xlab="", ylab="Global Active Power (kilowatts)") 
+lines(household$DateTime, household$Global_active_power, type="l")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -6,9 +6,9 @@ household$DateTime <- strptime(household$DateTime, "%Y-%m-%d %H:%M:%S")
 
 png("figure/plot3.png", width=480, height=480)
 
-plot(subHouseHold$DateTime, subHouseHold$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering") 
-lines(subHouseHold$DateTime, subHouseHold$Sub_metering_1, type="l", col="black")
-lines(subHouseHold$DateTime, subHouseHold$Sub_metering_2, type="l", col="red")
-lines(subHouseHold$DateTime, subHouseHold$Sub_metering_3, type="l", col="blue")
+plot(household$DateTime, household$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering") 
+lines(household$DateTime, household$Sub_metering_1, type="l", col="black")
+lines(household$DateTime, household$Sub_metering_2, type="l", col="red")
+lines(household$DateTime, household$Sub_metering_3, type="l", col="blue")
 legend("topright", lwd=1, col=c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
